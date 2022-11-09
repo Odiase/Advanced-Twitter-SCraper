47f73c6db14c240b037680659930e2acf050a1b0
GEp-Efosa <111660669+GEp-Efosa@users.noreply.github.com>
2022-11-09 15:18:01 +0100

    Added A Machine Learning Sentiment Analysis to tell if a tweet is positive or negative

--- tweets/app.py
+++ tweets/app.py
@@ -1,10 +1,28 @@
 import snscrape.modules.twitter as sntwitter
 from datetime import datetime
-
+from urllib.parse import urlencode, quote_plus
+import requests
+import math 
 
 from flask import Flask, render_template, request
 
+
+
 app = Flask(__name__)
+
+### Sentiment Data
+def sentiment_data(url):
+    response = requests.get(url).json()
+
+    negative = int(response["negative"] * 100)
+    positive = int(response["positive"] * 100)
+    
+    if positive > negative:
+        return f"{int(positive)}% Positive"
+    else:
+        return f"{int(negative)}% Negative"
+
+
 
 
 # function to get tweets from user's twitter handle
@@ -13,11 +31,20 @@ def get_tweets(handle, limit, date_from, date_to):
     tweet_limit = int(limit) -1
 
     tweets = []
+    sentiment_url = "https://api.uclassify.com/v1/uclassify/sentiment/classify?"
 
     for tweet in sntwitter.TwitterSearchScraper(query).get_items():
+        tweet_data = {}
         if len(tweets) <= tweet_limit:
             tweet = vars(tweet)
-            tweets.append(tweet)
+            payload = {"readKey" : "0hIf15vXIlcy", "text" : tweet}
+            payload = urlencode(payload, quote_via=quote_plus)
+            url = sentiment_url + payload
+            result = sentiment_data(url)
+
+            tweet_data["tweet"] = tweet
+            tweet_data["sentiment_analysis"] = result
+            tweets.append(tweet_data)
         else:
             break
             
